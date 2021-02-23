870a3a7f1ca80c8bd9f0051205bf2bc84b3027fe
your_rainey8507 <trangle.gg8507@gmail.com>
2021-02-22 21:25:16 -0600

    second commit

--- website/app.js
+++ website/app.js
@@ -1,5 +1,10 @@
 /* Global Variables */
+const baseURL ='api.openweathermap.org/data/2.5/weather?q=Oklahoma';
+const apiKey = '&appid=&appid=11117c86cac480a49daa024a4498118d'
 
+const date = document.getElementById('date');
+const temp = document.getElementById('temp');
+const content = document.getElementById('content');
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
