1359a432d1d9761b64d09dad3df12111ee1dd815
your_rainey8507 <trangle.gg8507@gmail.com>
2021-02-25 20:22:26 -0600

    second commit

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
   
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
 // Start up an instance of app
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const port = 3000;
 
 // To spin-up the server
 const server = app.listen(port, listening);
 function listening(){
     // console.log(server);
-    console.log(`running on localhost: ${PORT}`);
+    console.log(`running on localhost: ${port}`);
 };
 
-
-app.get('/all', function(req, res) {
+app.get('/add', sendData);
+function sendData (req, res) {
     res.send(projectData);
-}); 
+};
 
-
-app.post('/all', function (req, res) {
+/*app.post('/all', function (req, res) {
     res.send(projectData);
-});
+});*/
+
+// POST route 
+app.post('/addWeather', postData);
+function postData(req, res) {
+    console.log(req.body);
+    projectData = req.body;
+}
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));app.use(bodyParser.json());
--- website/app.js
+++ website/app.js
@@ -1,10 +1,76 @@
-/* Global Variables */
-const baseURL ='api.openweathermap.org/data/2.5/weather?q=Oklahoma';
-const apiKey = '&appid=&appid=11117c86cac480a49daa024a4498118d'
 
-const date = document.getElementById('date');
-const temp = document.getElementById('temp');
-const content = document.getElementById('content');
+
+/* Global Variables */
+//const api =`api.openweathermap.org/data/2.5/weather?q=${location}&appid=11117c86cac480a49daa024a4498118d`;
+//const zipCode = document.querySelector('#zip');
+//const feelings = document.querySelector('#feelings');
+const button = document.querySelector('#generate');
+
+const baseURL = 'api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&appid=c286b5e1dc71e3539a598870ffa96c2e';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let todayDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Button Click 
+document.getElementById('generate').addEventListener('click', getWeather);
+
+function getWeather(e) {
+    e.preventDefault();
+    const feelings = document.getElementById('feelings').value;
+    const zipCode = document.getElementById('zip').value;
+    getWeatherData(baseURL, apiKey, zipCode).
+    then(function(weatherData){
+        const temp = weatherData.main.temp;
+        const humidity = weatherData.main.humidity;
+        const userFeel = feelings;
+        postData('/add', {
+            temp,
+            humidity,
+            userFeel
+        }).then(() => {updateUI();})
+    });
+}
+
+const  getWeatherData = async (baseURL, apiKey, zipCode) => {
+    const res = await fetch(baseURL + apiKey + zipCode)
+    try {
+        const newWeatherData = await res.json();
+        console.log(newWeatherData);
+        return newWeatherData;
+    } catch {
+        console.log("error", error);
+    }
+}
+
+// POST function to server
+
+async function postData(url, data) {
+    await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    });
+}
+
+// Update UI 
+async function updateUI() {
+    const res = await fetch('/add');
+    const recentEntry = await res.json();
+    document.querySelector("#date").innerHTML = todayDate;
+    document.querySelector("#content").innerHTML = recentEntry.feelings;
+    document.querySelector('#temp').innerHTML = Math.floor(recentEntry.temp) + "C";
+}
+/*fetch(api).then((response) => {
+    return response.json();
+}).catch((error) => {
+    console.log(error);
+})*/
+
+//const date = document.getElementById('date');
+//const temp = document.getElementById('temp');
+//const content = document.getElementById('content');
+
+
+
